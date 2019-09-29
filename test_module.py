34b466342f6389f64835929297cc4aff306f5829
Aidar_Alimbayev <transcription@rambler.ru>
2019-09-30 00:35:36 +0600

    testing creating modules and classes

---
+++ test_module.py
@@ -0,0 +1,16 @@
+# define function
+def hello():
+    print("Hello, World!")
+
+# define a variable
+shark = "Sunny"
+
+# define a class
+class Octopus:
+    def __init__(self, name, color):
+        self.color = color
+        self.name = name
+    
+    def tell_me_about_the_octopus(self):
+        print("This octopus is " + self.color + ".")
+        print(self.name + " is the octopus's name.")  
