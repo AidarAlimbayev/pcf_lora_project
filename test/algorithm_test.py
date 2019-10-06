7796c1ceae41907c0d1eb027c6db9180e7e190cb
Aidar_Alimbayev <transcription@rambler.ru>
2019-10-06 18:20:57 +0600

    maint test algorithm

--- test/algorithm_test.py
+++ test/algorithm_test.py
@@ -1,33 +1,77 @@
 from datetime import datetime, date, time
+import time
+import socket
+import json
+import requests
+import binascii
+import csv
+import re
 
-
-id = 0
-weight_list = []
+cow_id = ''
+weight_list = [0]
 weight = 0
-date_now = str(datetime.now())
+weight_finall = 0
+date_now = ''
+type_scales = "Scale_A"
+row = []
 
 def connect_weight():
     print("Measure weight of cow")
-    weight = input("Enter weight: ")
-    if weight != 0:
+    weight = float(input("Enter weight: "))
+    while (float(weight) != 0):
         weight = input("Enter weight:_")
-        weight_list.append(weight)
+        weight_list.append(float(weight))
         #connect_weight()
-    else: 
-        print("Array:", weight_list)
+    del weight_list[-1]
+    if weight_list == 0:
         return(0)
+    else:
+        weight_finall =  sum(weight_list) / len(weight_list) 
+        weight_list = []
+        return(weight_finall)
     
 
 def connect_id():
     print("Try to connect RFID")
-    id = input("Enter ID: ")
-    if id == 0:
-        connect_id()
-    else:
-        return(id)
+    cow_id = str(input("Enter ID: "))
+    return(cow_id)
+    
+def send_server():
+    print("ID:", id)
+    print("Weight:", weight_finall)
+    print("Date:", str(datetime.now()))
+    print ("Scale type:", type_scales)
 
-connect_weight()
+def collect_data():
+    date_now = (str(datetime.now()))
+    #cow_id = '0'
+    #weight_finall = 0
+    row = [cow_id, weight_finall,  date_now, type_scales]
+    with open('test_algorithm.csv', 'a', newline='') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerow(row)
+    writeFile.close()
+    weight_finall = 0; 
+
+
+#weight_finall = connect_weight()
+#print("Weight finall:", weight_finall)
 
 #id = connect_id()
-
 #print(id)
+
+def main():
+    cow_id = connect_id()
+    if cow_id != 0:
+        weight_finall = connect_weight()
+        if weight_finall != 0:
+            send_server()
+            collect_data()
+        else:
+            #return(0)
+            main()
+    else:
+        #return(0)
+        main() 
+
+main()
