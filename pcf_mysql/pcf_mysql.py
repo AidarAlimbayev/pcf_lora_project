a9d5045b4a198838558ed56fa6fc05b1ef571748
AidarAlimbayev <transcription@rambler.ru>
2020-04-07 22:01:28 +0600

    _delete files

--- pcf_mysql/pcf_mysql.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "pcf_user_mysql",
-    passwd = "pcf_user_mysql"
-)
-
-print(mydb)
--- test_rfid_1/test_rfid_1/bin/Debug/EmptyCSourceFile.d
+++
@@ -1,2 +0,0 @@
-/home/aidar/Documents/test_rfid_1/test_rfid_1/bin/Debug/EmptyCSourceFile.o: \
- /home/aidar/Documents/test_rfid_1/test_rfid_1/EmptyCSourceFile.c
