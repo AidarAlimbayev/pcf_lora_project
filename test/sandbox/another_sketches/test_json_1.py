c4f46e2a3fc3c9b4aae4496da5dc7a150e40ed31
Aidar Alimbayev <transcription@rambler.ru>
2020-05-25 23:18:17 +0600

    test json worked variant

--- test/sandbox/another_sketches/test_json_1.py
+++ test/sandbox/another_sketches/test_json_1.py
@@ -24,15 +24,16 @@ import requests
 import json
 
 #url = 'http://87.247.28.238:8501/api/weights'
-url = 'http://194.4.56.86:8500/api/weights'
+url = 'http://194.4.56.86:8501/api/weights'
 headers = {'Content-type': 'application/json'}
 
-data = {"AnimalNumber" : "KZA120000000",
-        "Date" : "2020-05-25 T10:03:00",
+data = {"AnimalNumber" : "AZT120000010", 
+        "Date" : "2020-05-25T10:03:00",
         "Weight" : "555.5" ,
-        "ScalesModel" : "test_1"}  # Если по одному ключу находится несколько словарей, формируем список словарей
+        "ScalesModel" : "test_2"}  # Если по одному ключу находится несколько словарей, формируем список словарей
 
 answer = requests.post(url, data=json.dumps(data), headers=headers)
-print(answer)
-response = answer.json()
-print(response)
+print(answer) # можно ли как-то на этой строке остановиться вдебаге?
+#response = answer.json()
+print("_____________")
+#print(response.json())
