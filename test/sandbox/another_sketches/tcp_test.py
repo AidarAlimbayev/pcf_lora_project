eab74119ccfcdddc922a782a680512559ac7e0f9
AidarAlimbayev <transcription@rambler.ru>
2020-05-28 02:06:09 +0600

    new lib and new main

--- test/sandbox/another_sketches/tcp_test.py
+++ test/sandbox/another_sketches/tcp_test.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
    
 import socket
+import binascii
     
     
-TCP_IP = '192.168.0.250'
-TCP_PORT = 27001
+TCP_IP = '192.168.1.250'
+TCP_PORT = 60000
 BUFFER_SIZE = 1024
 #MESSAGE = "04ff211995"
-   
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(bytearray([0x06, 0x00, 0x01, 0x04, 0xff, 0xd4, 0x39]))
+#s.send(bytearray([0x53, 0x57, 0x00, 0x03, 0xff, 0xe0, 0x74])) # Active mode command
+s.send(bytearray([0x53, 0x57, 0x00, 0x06, 0xff, 0x01, 0x00, 0x00, 0x00, 0x50]))
 
-data = s.recv(BUFFER_SIZE)
+while 1:
+
+
+    data = s.recv(BUFFER_SIZE)
+    data= str(binascii.hexlify(data))
+    print("Old data")
+    print(data)
+    data = data[:-5] #Cutting the string from unnecessary information after 7 signs 
+    data = data[-12:] #Cutting the string from unnecessary information before 24 signs
+    print("Cutted data")       
+    print(len(data))
+    print(data)
+
+    
 s.close()
-
 
 #print repr(data)
 # print data.decode("utf-16")
