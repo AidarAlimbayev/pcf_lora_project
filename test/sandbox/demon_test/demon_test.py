ae7cc18a56beb6f73895656bbcf96182edc6e5e4
AidarAlimbayev <transcription@rambler.ru>
2020-06-01 21:54:24 +0600

    deamon run test script

--- test/sandbox/demon_test/demon_test.py
+++ test/sandbox/demon_test/demon_test.py
@@ -1,6 +1,6 @@
 import datetime
 current_time = datetime.datetime.now()
-name = '/home/pi/Desktop/MyStartUp/' + str(current_time) + '.txt'
+name = '/home/pi/demon_files/' + str(current_time) + '.txt'
 f= open(name,"w+")
 f.write("Текущее время загрузки Raspberry Pi " + str(current_time) + '\r\n')
 for i in range(10):
