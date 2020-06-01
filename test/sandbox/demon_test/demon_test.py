c2c669810ddc373aee8230908a26c5164b5d81ab
AidarAlimbayev <transcription@rambler.ru>
2020-06-01 21:08:58 +0600

    demon

--- test/sandbox/demon_test/demon_test.py
+++ test/sandbox/demon_test/demon_test.py
@@ -1,1 +1,8 @@
-import python-deamon
+import datetime
+current_time = datetime.datetime.now()
+name = '/home/pi/Desktop/MyStartUp/' + str(current_time) + '.txt'
+f= open(name,"w+")
+f.write("Текущее время загрузки Raspberry Pi " + str(current_time) + '\r\n')
+for i in range(10):
+     f.write("Это строка номер %d\r\n" % (i+1))
+f.close()
