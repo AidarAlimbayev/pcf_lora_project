0ccb1f7eefb07421eb92219449dada4f2bb5a9d6
M100ika <maxat.suieubayev@gmail.com>
2022-02-16 16:21:14 +0600

    bash.first push

--- raspberry.sh
+++ raspberry.sh
@@ -1,43 +1,28 @@
 #!/usr/bin/env bash 
 set -Eeuo pipefail
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-#Установка Git
-apt-get install git
-
-
-apt update
-apt -y upgrade
+sudo apt update
+sudo apt -y upgrade
 echo `python3 -V`
-apt install -y python3-pip
+sudo apt install -y python3-pip
 echo `python3 -V`
 echo 'The Python has loaded'
 echo '###########################################################################################'
-for ((i = 1; i <= 3; i++))
-do
-echo "...$i"
-sleep 1
-done 
+sleep 3
 
-cd ~/Documents/
-#git clone https://github.com/AidarAlimbayev/pcf_lora_project.git  ds;fl;sdf
-#cp pcf_lora_project/software/main/main_pcf_ver4.py /home/pi/
-#cp pcf_lora_project/software/main/lib_pcf_ver4.py /home/pi/
-cp pcf_lora_project/software/main/main_aid_ver3.py /home/pi/
-cp pcf_lora_project/software/main/main_lib_ver3.py /home/pi/
-cp pcf_lora_project/software/main/pcf.service /etc/systemd/system/
-cp pcf_lora_project/software/main/99-serial-logger.rules /etc/udev/rules.d/
+cd Documents/
+git clone https://github.com/AidarAlimbayev/pcf_lora_project.git
+sudo cp pcf_lora_project/software/main/main_pcf_ver4.py /home/pi/
+sudo cp pcf_lora_project/software/main/lib_pcf_ver4.py /home/pcf/
+sudo cp pcf_lora_project/software/main/pcf.service /etc/systemd/system/
+sudo cp pcf_lora_project/software/main/99-serial-logger.rules /etc/udev/rules.d/
 
 
 
 echo 'Project has loaded'
 echo '###########################################################################################'
-for ((i = 1; i <= 3; i++))
-do
-echo "...$i"
-sleep 1
-done 
+sleep 3
 
 pip install serial          #import serial
 pip install python-time     #import time
@@ -53,17 +38,8 @@ pip install statistics      #import statistics
 
 echo 'All libraries has loaded'
 echo '###########################################################################################'
-for ((i = 1; i <= 3; i++))
-do
-echo "...$i"
-sleep 1
-done 
-
-
-#cd "/dev"
-#chmod +x ttyUSB0
-#chmpd +x ttyASM0 
+sleep 3
 
 
 
-#merge git узнать
+
