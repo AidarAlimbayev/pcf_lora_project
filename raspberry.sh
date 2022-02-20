ee070a2c400bd003113c2ed738f1414aa8e6f84e
M100ika <maxat.suieubayev@gmail.com>
2022-02-20 17:09:13 +0600

    raspberry.sh

--- raspberry.sh
+++ raspberry.sh
@@ -22,17 +22,13 @@ sleep 1
 done 
 
 #cd ~/Documents/
-<<<<<<< HEAD
 #git clone https://github.com/AidarAlimbayev/pcf_lora_project.git  
-=======
-#git clone https://github.com/AidarAlimbayev/pcf_lora_project.git  
->>>>>>> Maxat
-#cp pcf_lora_project/software/main/main_pcf_ver4.py /home/pi/
-#cp pcf_lora_project/software/main/lib_pcf_ver4.py /home/pi/
-cp pcf_lora_project/software/main/main_aid_ver3.py /home/pi/
-cp pcf_lora_project/software/main/main_lib_ver3.py /home/pi/
-cp pcf_lora_project/software/main/pcf.service /etc/systemd/system/
-cp pcf_lora_project/software/main/99-serial-logger.rules /etc/udev/rules.d/
+cp software/main/main_pcf_ver4.py /home/pi/
+cp software/main/lib_pcf_ver4.py /home/pi/
+#cp pcf_lora_project/software/main/main_aid_ver3.py /home/pi/
+#cp pcf_lora_project/software/main/main_lib_ver3.py /home/pi/
+cp software/main/pcf.service /etc/systemd/system/
+cp software/main/99-serial-logger.rules /etc/udev/rules.d/
 
 
 
@@ -44,17 +40,17 @@ echo "...$i"
 sleep 1
 done 
 
-#pip install serial          #import serial
-#pip install python-time     #import time
-#pip install sockets         #import socket
+pip install serial          #import serial
+pip install python-time     #import time
+pip install sockets         #import socket
 #pip install jsonlib         # - Не уверен /import json 
 #pip install requests        #import requests
 #pip install pycopy-binascii #import binascii
-#pip install python-csv      #import csv
-#pip insatll regex           #import re
-#pip install logging         #import logging
+pip install python-csv      #import csv
+pip insatll regex           #import re
+pip install logging         #import logging
 #pip install os-sys          #import os
-#pip install statistics      #import statistics
+pip install statistics      #import statistics
 
 echo 'All libraries has loaded'
 echo '###########################################################################################'
@@ -65,7 +61,7 @@ sleep 1
 done 
 
 
-#cd "/dev"
+#cd /dev
 #chmod +x ttyUSB0
 #chmpd +x ttyASM0 
 
