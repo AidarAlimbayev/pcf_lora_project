1b80d9ff5616abe4df6628904909477dbd54f8f5
M100ika <maxat.suieubayev@gmail.com>
2022-02-24 03:02:08 +0600

    Added Arduino Install

--- software/raspberry_settings/raspberry.sh
+++ software/raspberry_settings/raspberry.sh
@@ -8,13 +8,18 @@ apt update                          #Проверка обновлении
 apt -y upgrade                      #Установка обновлении 
 apt-get dist-upgrade                #Обновление raspbian до последней версии
 
-apt-get install git                 #Установка Git
-apt-get install tightvncserver      #Установка VNC
+#Установка Git
+apt-get install git
 
+#Установка VNC
+apt-get install tightvncserver      
+
+#Установка TeamViewer
 #wget https://www.teamviewer.com/ru/%d1%81%d0%ba%d0%b0%d1%87%d0%b0%d1%82%d1%8c/raspberry-pi/teamviewer_15.26.4_armhf.deb
 #dpkg -i teamviewer-host_armhf.deb
-#apt --fix-broken install           #Установка TeamViewer
+#apt --fix-broken install           
 
+#Установка Python
 echo `python3 -V`
 apt install python2
 apt install -y python3-pip
@@ -27,6 +32,7 @@ echo "...$i"
 sleep 1
 done 
 
+#Установка проекта
 #cd ~/Documents/
 #git clone git@github.com:AidarAlimbayev/pcf_lora_project.git
 #cp software/main/main_pcf_ver4.py /home/pi/
@@ -36,13 +42,13 @@ done
 #cp software/main/pcf.service /etc/systemd/system/
 #cp software/main/99-serial-logger.rules /etc/udev/rules.d/
 
-echo 'Project has loaded'
-echo '###########################################################################################'
-for ((i = 1; i <= 3; i++))
-do
-echo "...$i"
-sleep 1
-done 
+# echo 'Project has loaded'
+# echo '###########################################################################################'
+# for ((i = 1; i <= 3; i++))
+# do
+# echo "...$i"
+# sleep 1
+# done 
 
 pip install serial          #import serial
 pip install python-time     #import time
@@ -64,12 +70,26 @@ echo "...$i"
 sleep 1
 done 
 
+#Установка Arduino IDE. *Примечание, перед установкой соединить с raspberry
+
+mkdir ${HOME}/arduino
+cd ${HOME}/arduino/
+wget https://downloads.arduino.cc/arduino-1.8.15-linux64.tar.xz
+tar -xvf ./arduino-1.8.15-linux64.tar.xz
+cd ${HOME}/arduino/arduino-1.8.15/
+./install.sh
+if [ -f /dev/ttyASM* ]
+then 
+    chmod 777 ttyASM*
+elif [ -f /dev/ttyUSB* ]
+then
+    chmod 777 ttyUSB*
+else    
+    echo "Подключитесь к Arduino"
+    sleep 3
+fi
 
 
-
-#cd /dev
-#chmod +x ttyUSB0
-#chmpd +x ttyASM0 
 
 
 
