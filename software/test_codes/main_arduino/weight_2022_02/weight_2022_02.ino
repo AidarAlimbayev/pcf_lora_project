5d1dc4bd4754d607d2654bd50a7a1df4c08a11b6
AidarAlimbayev <transcription@rambler.ru>
2022-04-21 21:42:36 +0600

    save progress

--- software/test_codes/main_arduino/weight_2022_02/weight_2022_02.ino
+++ software/test_codes/main_arduino/weight_2022_02/weight_2022_02.ino
@@ -42,11 +42,15 @@ void setup() {
   //Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
 
   //Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
+  //Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
 						// by the SCALE parameter (not set yet)
 
   //scale.set_scale(2990.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+<<<<<<< HEAD
   scale.set_scale(4149.f);
+=======
+  scale.set_scale(2923.f);
+>>>>>>> fb83b77 (save progress)
   scale.tare();				        // reset the scale to 0
 
   //Serial.println("After setting up the scale:");
@@ -73,7 +77,7 @@ void loop() {
   //Serial.print("one reading:\t");
   //Serial.print(scale.get_units(), 1);
   //Serial.print("\t| average:\t");
-  Serial.println(scale.get_units(10), 1);
+  Serial.println(scale.get_units(10), 0);
 
   //scale.power_down();			        // put the ADC in sleep mode
   delay(1000);
