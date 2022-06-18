140c776baa9373243a2cdc0b970aec6b7f36a72f
Meiirkhan Abdek <ameyrkhan@gmail.com>
2022-06-18 11:30:39 +0600

    smooth start was added

--- software/main/lib_pcf_ver4.py
+++ software/main/lib_pcf_ver4.py
@@ -229,9 +229,15 @@ def PWM_GPIO_RASP(power,duration): #function creates pwm signal on 13th pin of t
     pi_pwm = GPIO.PWM(GPIO_PWM_0, 100)
     pi_pwm.start(0)
 
-    pi_pwm.ChangeDutyCycle(power)
+    for x in range (int(power/10)):
+        pi_pwm.ChangeDutyCycle(power/10*x)
+        sleep(0.2)
+
     sleep(duration)
-    pi_pwm.ChangeDutyCycle(0)
+    
+    for x in range (int(power/10)):    
+        pi_pwm.ChangeDutyCycle(power/10*(10-x))
+        sleep(0.2)
 
     pi_pwm.stop()
 
---
+++ software/main/switch.py
@@ -0,0 +1,23 @@
+import time
+import smtplib
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, GPIO.PUD_DOWN)
+
+GPIO_PWM_0 = 13
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(GPIO_PWM_0, GPIO.OUT)
+
+pi_pwm = GPIO.PWM(GPIO_PWM_0, 100)
+
+while True:
+    if GPIO.input(23):
+        pi_pwm.start(0)
+        pi_pwm.ChangeDutyCycle(50)
+    else:
+        pi_pwm.ChangeDutyCycle(0)
+
+        pi_pwm.stop()
