5e8b7c9d86370b97d72f02ad4e868fb20df2cabe
AidarAlimbayev <transcription@rambler.ru>
2022-09-11 13:35:56 +0600

    debug feeder

--- software/feeder_software/feeder_test.py
+++ software/feeder_software/feeder_test.py
@@ -94,7 +94,7 @@ def distance():
  
 def measuring_start(distance):
     try:
-        if distance <= 10:
+        if distance <= 50:
             return True
         else:
             return False
@@ -129,7 +129,6 @@ def Connect_RFID_reader():                                      # Connection to 
         logger.debug(f'START Null id null_id : {null_id}')
     
         if animal_id == null_id: # Send command to reader waiting id of animal
-            print("animal_id == null_id")
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((TCP_IP, TCP_PORT))
             s.send(bytearray([0x53, 0x57, 0x00, 0x06, 0xff, 0x01, 0x00, 0x00, 0x00, 0x50])) #Chafon RU5300 Answer mode reading mode command
@@ -154,14 +153,9 @@ def Connect_RFID_reader():                                      # Connection to 
 
 def rfid_label():
     try:
-        print("start rfid_label function")
         labels = []
-        
         while len(labels) <= 11:
-            print("while rfid label")
             cow_id = Connect_RFID_reader()
-            print("cow id")
-            print(cow_id)
             labels.append(cow_id)
         animal_id = max([j for i,j in enumerate(labels) if j in labels[i+1:]]) if labels != list(set(labels)) else -1
         return animal_id
