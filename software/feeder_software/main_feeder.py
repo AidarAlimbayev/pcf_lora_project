12eae568da38a8124c697e07ff61b39d0386fb82
AidarAlimbayev <transcription@rambler.ru>
2023-01-20 16:40:46 +0600

    change main

--- software/feeder_software/main_feeder.py
+++ software/feeder_software/main_feeder.py
@@ -42,16 +42,6 @@ null_id = "b'435400040001'"
 
 @logger.catch
 def main():
-        # Connection to arduino
-    try:
-        s = serial.Serial('/dev/ttyACM0',9600) # path inside rapberry pi to arduino into dev folder
-        logger.info(f'Connect arduino {s.name}')
-        logger.info(f'Configuration of serial, {s}')
-    except Exception as e:
-        logger.info(f'Error to connection to arduino, there is no file: /dev/ttyACM0 {e}')
-    else:
-        logger.info(f'Success: Arduino connected')
-    
 
     print("------------Start main function------------------")
     GPIO.setmode(GPIO.BCM)  
@@ -60,6 +50,17 @@ def main():
     choice = fdr.input_with_timeout("Choice:", 5)
     time.sleep(5)
     i = 0
+
+            # Connection to arduino
+    try:
+        s = serial.Serial('/dev/ttyACM0',9600) # path inside rapberry pi to arduino into dev folder
+        logger.info(f'Connect arduino {s.name}')
+        logger.info(f'Configuration of serial, {s}')
+    except Exception as e:
+        logger.info(f'Error to connection to arduino, there is no file: /dev/ttyACM0 {e}')
+    else:
+        logger.info(f'Success: Arduino connected')
+
     if choice == '1':
         offset, scale = fdr.calibrate()
         cfg.update_setting("Calibration", "Offset", offset)
